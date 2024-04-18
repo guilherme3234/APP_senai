36c3153b3f6286a757228898d61bc97523e71d7b
guilherme3234 <140507408+guilherme3234@users.noreply.github.com>
2024-04-18 14:08:21 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,19 +1,1 @@
 
-import {View} from 'react-native';
-import React from 'react';
-
-import Inventarios from './Screens/inventarios';
-import DescItens from './Screens/descItens';
-import CadastroItens from './Screens/cadastroItens';
-
-
-
-const App = () => {
-  return (
-    <View style={{flex: 1}}>
-      <CadastroItens />
-    </View>
-
-  );
-  }
-export default App;
